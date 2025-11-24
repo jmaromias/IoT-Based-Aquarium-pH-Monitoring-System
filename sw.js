0bd26d866ae7407726917e9f952f5b670bbec091
jmaromias <jmaromias@unp.edu.ph>
2025-11-25 00:39:07 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'aqua-status-v1';
+const FILES = [
+  './',
+  './index.html',
+  './logo.jpg',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(FILES)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => r || fetch(e.request))
+  );
+});
